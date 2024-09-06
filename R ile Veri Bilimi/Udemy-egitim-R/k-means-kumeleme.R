d6b7f797d3d3f064a2f3fb9ed3cd49c86b6c4986
halilkkaya <kaya_zara@hotmail.com>
2024-09-06 20:23:23 +0300

    Update k-means-kumeleme.R

--- R ile Veri Bilimi/Udemy-egitim-R/k-means-kumeleme.R
+++ R ile Veri Bilimi/Udemy-egitim-R/k-means-kumeleme.R
@@ -152,6 +152,7 @@ reversedData$clusterK2 <- clusterModelK2$cluster
 reversedData$clusterK3 <- clusterModelK3$cluster
 a
 b
+c
 reversedData %>% group_by(clusterK2) %>% summarise_all(mean)
 # video 332 anlatiyor
 # kumelerimin arasindaki mesafe fazla anlamli bi kumeleme duruyor
